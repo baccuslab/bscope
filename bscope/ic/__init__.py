067f622efe664466cfd5d7351640039f56e26dea
Joshua B Melander <melander@stanford.edu>
2025-06-19 18:32:59 +0000

    update

--- bscope/ic/__init__.py
+++ bscope/ic/__init__.py
@@ -1,5 +1,5 @@
 from .models import *
 from .evaluation import *
-from .contribution_loader import *
 from .semantic_analyzer import *
-from .load_contribution_data import CustomImageNetDataset
+from .load_contribution_data import *
+from .custom_dataset import CustomImageNetDataset
